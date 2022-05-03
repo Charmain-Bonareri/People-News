75049b9f0dfbf7eba1ded0560f3dad28ac7c376a
Charmain <mogerecharmain@gmail.com>
2022-05-03 06:13:55 +0300

    Created a new macros file to store all our macros.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from  .request import get_sources
 
 # Views
 @app.route('/')
@@ -8,5 +9,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    sources = get_sources()
     title = 'People News'
-    return render_template('index.html')
+    return render_template('index.html', title = title, sources = sources)
