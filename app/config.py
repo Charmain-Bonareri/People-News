fc2d16b2aab48e6d34760646c1f7f1a315e11e1f
Charmain <mogerecharmain@gmail.com>
2022-05-03 05:30:46 +0300

    Created another configuration file that will store our API key.

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL="https://newsapi.org/v2/{}?country=us&apiKey={}"
 
 
 
--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'People News'
     return render_template('index.html')
